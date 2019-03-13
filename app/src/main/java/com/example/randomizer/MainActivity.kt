163cc75173e2bac7af446678b4306a531b6fff1b
TaaviMeinberg <taavi.meinberg@gmail.com>
2019-03-13 21:03:01 +0200

    Added number to show selected progressBar value, fixed logic error

--- app/src/main/java/com/example/randomizer/MainActivity.kt
+++ app/src/main/java/com/example/randomizer/MainActivity.kt
@@ -6,6 +6,10 @@ import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
 import java.util.*
+import android.widget.Toast
+import android.widget.SeekBar.OnSeekBarChangeListener
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +20,30 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton = findViewById<Button>(R.id.rollButton)
         val resultsView = findViewById<TextView>(R.id.resultsTextView)
+        val highestSelectedValue = findViewById<TextView>(R.id.highestSelectedValue)
 
         val seekBar = findViewById<SeekBar>(R.id.seekBar)
 
+        highestSelectedValue.text = seekBar.progress.toString()
         rollButton.setOnClickListener {
-            val rand = Random().nextInt(seekBar.progress)+1
+            val rand = Random().nextInt(seekBar.progress+1)
 
             resultsView.text = rand.toString()
         }
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // TODO Auto-generated method stub
+                highestSelectedValue.text = progress.toString()
+            }
+        });
     }
 }
